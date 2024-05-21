71f8055def8d9d399a7b6d2a1cef1f3f875a916d
Marina Moiseenko <moiseenko.marina07@gmail.com>
2024-05-21 12:33:28 +0300

    Разделяй и властвуй. Рефакторинг

--- src/view/edit-form-view.js
+++ src/view/edit-form-view.js
@@ -2,90 +2,79 @@ import { CITIES, POINT_TYPES, TIME_FORMAT, FULL_DATE_FORMAT } from '../const.js'
 import { transformDate, formatDateInForm } from '../utils.js';
 import AbstractView from '../framework/view/abstract-view.js';
 
-function createEditFormTemplate(point, destinations, offers) {
-  const {
-    basePrice: price,
-    dateFrom: dateFrom,
-    dateTo: dateTo,
-    destination,
-    type: offerType,
-    offers: offersList,
-    id,
-  } = point;
-  const pointDestination = destinations.find((dest) => dest?.id === destination);
-  const typeOffers = offers.find((offer) => offer?.type === offerType).offers;
-  const pointOffers = typeOffers.filter((typeOffer) => offersList.includes(typeOffer?.id));
-  const renderRoutesTypes = () => POINT_TYPES.map((type) => `
-  <div class="event__type-item">
-    <input
-      id="event-type-${type}-${id}"
-      class="event__type-input
-      visually-hidden"
-      type="radio"
-      name="event-type"
-      value="${type}"
-      ${offerType === type ? 'checked' : ''}
-    >
-    <label
-      class="event__type-label event__type-label--${type}"
-      for="event-type-${type}-${id}"
-    >${transformDate(type)}</label>
-  </div>`).join('');
+function renderRoutesTypes(id, offerType) {
+  return POINT_TYPES.map((type) => `
+        <div class="event__type-item">
+          <input
+            id="event-type-${type}-${id}"
+            class="event__type-input visually-hidden"
+            type="radio"
+            name="event-type"
+            value="${type}"
+            ${offerType === type ? 'checked' : ''}
+          >
+          <label class="event__type-label event__type-label--${type}" for="event-type-${type}-${id}">
+            ${transformDate(type)}
+          </label>
+        </div>
+      `
+  ).join('');
+}
 
-  const renderOffersTypes = (offersTypes) => {
-    if (offersTypes.length !== 0) {
-      return `
+function renderOffersTypes(offersTypes, pointOffers, id) {
+  if (offersTypes.length !== 0) {
+    return `
         <section class="event__section  event__section--offers">
           <h3 class="event__section-title  event__section-title--offers">Offers</h3>
           <div class="event__available-offers">
-          ${offersTypes.map((offerTypes) => `
+          ${offersTypes.map((offerType) => `
           <div class="event__offer-selector">
             <input
-              class="event__offer-checkbox
-              visually-hidden"
-              id="event-offer-${offerTypes.title}-${id}"
+              class="event__offer-checkbox visually-hidden"
+              id="event-offer-${offerType.title}-${id}"
               type="checkbox"
-              name="event-offer-${offerTypes.title}"
-              ${pointOffers.includes(offerTypes) ? 'checked' : ''}
+              name="event-offer-${offerType.title}"
+              ${pointOffers.includes(offerType) ? 'checked' : ''}
             >
-            <label class="event__offer-label" for="event-offer-${offerTypes.title}-${id}">
-              <span class="event__offer-title">${offerTypes.title}</span>
+            <label class="event__offer-label" for="event-offer-${offerType.title}-${id}">
+              <span class="event__offer-title">${offerType.title}</span>
               &plus;&euro;&nbsp;
-              <span class="event__offer-price">${offerTypes.price}</span>
+              <span class="event__offer-price">${offerType.price}</span>
             </label>
           </div>`).join('')}
           </div>
         </section>`;
-    } else {
-      return '';
-    }
-  };
+  } else {
+    return '';
+  }
+}
 
-  const renderPointDestination = (pointDest) => {
-    const description = pointDest.description;
-    const pictures = pointDest.pictures;
+function renderPointDestination(id, pointDest) {
+  const description = pointDest.description;
+  const pictures = pointDest.pictures;
 
-    if (description !== '' && pictures.length !== 0) {
-      return `
+  if (description !== '' && pictures.length !== 0) {
+    return `
         <section class="event__section  event__section--destination">
           <h3 class="event__section-title  event__section-title--destination">Destination</h3>
           <p class="event__destination-description">${description}</p>
           ${pictures.length !== 0 ? `
             <div class="event__photos-container">
               <div class="event__photos-tape">
-                ${pictures.map((picture) => `<img class="event__photo" src=${picture.src} alt=${picture.description}>`).join('')}
+                ${pictures.map((picture) => `<img class="event__photo" src="${picture.src}" alt="${picture.description}">`).join('')}
               </div>
             </div>
           ` : ''}
         </section>`;
-    } else {
-      return '';
-    }
-  };
+  } else {
+    return '';
+  }
+}
 
-  const renderCityOptionsList = () => CITIES.map((city) => `<option value="${city}"></option>`).join('');
+const renderCityOptionsList = () => CITIES.map((city) => `<option value="${city}"></option>`).join('');
 
-  const renderTypeWrapper = () => `
+function renderTypeWrapper(id, routesTypesMarkup, offerType) {
+  return `
     <div class="event__type-wrapper">
       <label class="event__type  event__type-btn" for="event-type-toggle-${id}">
         <span class="visually-hidden">Choose event type</span>
@@ -95,12 +84,14 @@ function createEditFormTemplate(point, destinations, offers) {
       <div class="event__type-list">
         <fieldset class="event__type-group">
           <legend class="visually-hidden">Event type</legend>
-          ${renderRoutesTypes()}
+          ${routesTypesMarkup}
         </fieldset>
       </div>
     </div>`;
+}
 
-  const renderEventFieldGroups = () => `
+function renderEventFieldGroups(id, dateFrom, dateTo, price, offerType, pointDestination) {
+  return `
       <div class="event__field-group  event__field-group--destination">
         <label class="event__label event__type-output" for="event-destination-${id}">
           ${transformDate(offerType)}
@@ -110,18 +101,17 @@ function createEditFormTemplate(point, destinations, offers) {
           id="event-destination-${id}"
           type="text"
           name="event-destination-${id}"
-          value=${pointDestination?.name}
+          value="${pointDestination?.name}"
           list="destination-list-${id}"
         >
         <datalist id="destination-list-${id}">
-          ${renderCityOptionsList()}
+          ${renderCityOptionsList(id)}
         </datalist>
       </div>
       <div class="event__field-group  event__field-group--time">
         <label class="visually-hidden" for="event-start-time-${id}">From</label>
         <input
-          class="event__input
-          event__input--time"
+          class="event__input event__input--time"
           id="event-start-time-${id}"
           type="text"
           name="event-start-time"
@@ -130,8 +120,7 @@ function createEditFormTemplate(point, destinations, offers) {
         &mdash;
         <label class="visually-hidden" for="event-end-time-${id}">To</label>
         <input
-          class="event__input
-          event__input--time"
+          class="event__input event__input--time"
           id="event-end-time-${id}"
           type="text"
           name="event-end-time"
@@ -143,14 +132,35 @@ function createEditFormTemplate(point, destinations, offers) {
           <span class="visually-hidden">Price</span>
           &euro;
         </label>
-        <input class="event__input  event__input--price" id="event-price-${id}" type="text" name="event-price" value="${price}">
+        <input class="event__input event__input--price" id="event-price-${id}" type="text" name="event-price" value="${price}">
       </div>`;
+}
+
+function createEditFormTemplate(point, destinations, offers) {
+  const {
+    basePrice: price,
+    dateFrom,
+    dateTo,
+    destination,
+    type: offerType,
+    offers: offersList,
+    id,
+  } = point;
+  const pointDestination = destinations.find((dest) => dest?.id === destination);
+  const typeOffers = offers.find((offer) => offer?.type === offerType).offers;
+  const pointOffers = typeOffers.filter((typeOffer) => offersList.includes(typeOffer?.id));
+
+  const routesTypesMarkup = renderRoutesTypes(id, offerType);
+  const routesTypesWrapperMarkup = renderTypeWrapper(id, routesTypesMarkup, offerType);
+  const offersTypesMarkup = renderOffersTypes(typeOffers, pointOffers, id);
+  const pointDestinationMarkup = renderPointDestination(id, pointDestination);
+  const eventFieldGroupsMarkup = renderEventFieldGroups(id, dateFrom, dateTo, price, offerType, pointDestination);
 
   return (
     `<form class="event event--edit" action="#" method="post">
         <header class="event__header">
-          ${renderTypeWrapper()}
-          ${renderEventFieldGroups()}
+          ${routesTypesWrapperMarkup}
+          ${eventFieldGroupsMarkup}
           <button class="event__save-btn  btn  btn--blue" type="submit">Save</button>
           <button class="event__reset-btn" type="reset">Delete</button>
           <button class="event__rollup-btn" type="button">
@@ -158,8 +168,8 @@ function createEditFormTemplate(point, destinations, offers) {
           </button>
         </header>
         <section class="event__details">
-          ${renderOffersTypes(pointOffers)}
-          ${renderPointDestination(pointDestination)}
+          ${offersTypesMarkup}
+          ${pointDestinationMarkup}
       </section>
     </form>`
   );
